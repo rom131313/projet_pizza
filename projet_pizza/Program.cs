d8b7ed92345737de2e789fb3685b0a0d26d5f843
halison andrianaivoarivelo <halison.andria@gmail.com>
2025-02-07 16:51:44 +0100

    129. Exercice : Trier les pizzas par prix

--- projet_pizza/Program.cs
+++ projet_pizza/Program.cs
@@ -1,67 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
-namespace MyApp
+namespace projet_pizza
 {
     class Pizza
     {
         string nom;
-        float prix;
+        public float prix { get; private set; }
         bool vegetarienne;
+        List<string> ingredients;
 
-        public Pizza(string nom, float prix, bool vegetarienne)
+        public Pizza(string nom, float prix, bool vegetarienne, List<string> ingredients)
         {
             this.nom = nom;
             this.prix = prix;
             this.vegetarienne = vegetarienne;
+            this.ingredients = ingredients;
         }
 
         public void Afficher()
         {
+            //4 fromages (V) - 11.5€
             /*string badgeVegetarienne = " (V)";
             if (!vegetarienne)
             {
                 badgeVegetarienne = "";
-            }
-            Console.WriteLine(nom + badgeVegetarienne +  " - " + prix + "€");*/
+            }*/
+
             string badgeVegetarienne = vegetarienne ? " (V)" : "";
-            Console.WriteLine(nom + badgeVegetarienne + " - " + prix + "€");
+
+            string nomAfficher = FormatPremiereLettreMajuscules(nom);
+
+            /*var ingredientsAfficher = new List<string>();
+            foreach(var ingredient in ingredients)
+            {
+                ingredientsAfficher.Add(FormatPremiereLettreMajuscules(ingredient));
+            }*/
+
+            var ingredientsAfficher = ingredients.Select(i => FormatPremiereLettreMajuscules(i)).ToList();
+
+            Console.WriteLine(nomAfficher + badgeVegetarienne + " - " + prix + "€");
+            Console.WriteLine(string.Join(", ", ingredientsAfficher));
+            Console.WriteLine();
+
         }
 
+        private static string FormatPremiereLettreMajuscules(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
 
+            string minuscules = s.ToLower();
+            string majuscules = s.ToUpper();
+
+            string resultat = majuscules[0] + minuscules[1..];
+
+            return resultat;
+        }
     }
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            // Clas Pizza
-            // nom (4 fromages)
-            // prix : 11.5
-            // vegetarienne (vrai ou faux)
-            // Constructeur 
+            // ingredients <- Liste string   "mozarella"    "poulet"   "oignon"
+            // données
             // Afficher
-            // 4 fromages (V) - 11.5
-            // Créer une liste de pizzas
-            // boucler pour afficher les pizzas
+            // 4 fromages (V) - 11.5€
+            // cantal, mozzarella, gruyère, conté
+            //
+            // ...
+            // join
 
-            //var pizza1 = new Pizza("4 fromages", 11.5f, true);
-            //pizza1.Afficher();
 
-            var listePizzas = new List<Pizza>()
-            {
-                new Pizza("4 fromages", 11.5f, true),
-                new Pizza("indienne", 10.5f, false),
-                new Pizza("mexicaine", 13f, false),
-                new Pizza("margherita", 8f, true),
-                new Pizza("calzone", 12f, false),
-                new Pizza("complète", 9.5f, false),
+            var pizzas = new List<Pizza>() {
+                new Pizza("4 fromages", 11.5f, true, new List<string> { "cantal", "mozzarella", "fromage de chèvre", "gruyère" }),
+                new Pizza("indienne", 10.5f, false, new List<string> { "curry", "mozzarella", "poulet", "poivron", "oignon", "coriandre" }),
+                new Pizza("MEXICAINE", 13f, false, new List<string> {"boeuf", "mozzarella", "maïs", "tomates", "oignon", "coriandre"}),
+                new Pizza("margherita", 8f, true, new List<string> { "sauce tomate", "mozzarella", "basilic" }),
+                new Pizza("Calzone", 12f, false, new List<string> { "tomate", "jambon", "persil", "oignons"}),
+                new Pizza("complète", 9.5f, false, new List<string> { "jambon", "oeuf", "fromage" }),
             };
 
-            foreach (var pizza in listePizzas)
+            pizzas = pizzas.OrderBy(p => p.prix).ToList();
+
+            foreach (var pizza in pizzas)
             {
-                pizza.Afficher(); 
+                pizza.Afficher();
             }
+
         }
     }
 }
