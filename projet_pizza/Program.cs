97c64aae23651b858970f37046b40803b72e4dca
halison andrianaivoarivelo <halison.andria@gmail.com>
2025-02-07 19:31:12 +0100

    133. Exercice : Pizza personnalisée

--- projet_pizza/Program.cs
+++ projet_pizza/Program.cs
@@ -5,12 +5,42 @@ using System.Text;
 
 namespace projet_pizza
 {
+
+    // PizzaPersonnalise 
+    // - CLasse : hériter de Pizza 
+    //:constructeur 
+    // nom = "Personnalisée"
+    // prix - 5
+    // vegetarienne - false 
+    // ingrédients ) demander à l'utilisateur 
+    // Rentrez un ingrédient pour la pizza personalisée (ENTER pour terminer) : 
+
+    class PizzaPersonnalisee : Pizza
+    {
+        public PizzaPersonnalisee(): base("Personnalisee", 5, false, null)
+        {
+            ingredients = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Rentrez un ingrédient pour la pizza personnalisée (ENTER pour terminer) : ");
+                var ingredient = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(ingredient))
+                {
+                    break;
+                }
+                ingredients.Add(ingredient);
+            }
+
+        }
+    }
+
     class Pizza
     {
         string nom;
         public float prix { get; private set; }
         public bool vegetarienne { get; private set; }
-        public List<string> ingredients;
+        public List<string> ingredients { get; protected set; }
 
         public Pizza(string nom, float prix, bool vegetarienne, List<string> ingredients)
         {
@@ -88,10 +118,12 @@ namespace projet_pizza
                 new Pizza("margherita", 8f, true, new List<string> { "sauce tomate", "mozzarella", "basilic" }),
                 new Pizza("Calzone", 12f, false, new List<string> { "tomate", "jambon", "persil", "oignons"}),
                 new Pizza("complète", 9.5f, false, new List<string> { "jambon", "oeuf", "fromage" }),
+                new PizzaPersonnalisee()
+
             };
 
             //pizzas = pizzas.Where(p => p.vegetarienne).ToList();
-            pizzas = pizzas.Where(p => p.ContientIngredient("oignon")).ToList();
+            //pizzas = pizzas.Where(p => p.ContientIngredient("oignon")).ToList();
 
 
             foreach (var pizza in pizzas)
